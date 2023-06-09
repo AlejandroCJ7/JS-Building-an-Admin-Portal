4d3e0873fbdb0b32b89e572d676dd087e6656d0b
Alex Castaneda <alex@Alejandros-Mac.local>
2023-06-09 13:30:42 -0700

    first commit

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,10 @@
 
 // Your Code Here
+
+//An asynchronous function that fetches a list of books from the server and returns the parsed JSON result
+
+async function listBooks() {
+    const response = await fetch("http://localhost:3001/listBooks");
+    const result = await response.json();
+    return result;
+      }
