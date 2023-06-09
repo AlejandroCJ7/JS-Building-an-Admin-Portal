f78c8f7f83354954ec842dba71d085912c3a426c
Alex Castaneda <alex@Alejandros-Mac.local>
2023-06-09 15:42:38 -0700

    admin.js is working and entering the quantity for a book is updating on the books page

--- public/admin.js
+++ public/admin.js
@@ -8,3 +8,44 @@ async function listBooks() {
     const result = await response.json();
     return result;
       }
+
+
+//added async function to create a lsit of books
+      async function buildAdminList() {
+        let listOfBooks = await listBooks();
+          
+        let list = document.createElement("ul");
+            list.setAttribute("id", "list");
+        for (let i = 0; i < listOfBooks.length; i++) {
+        let inputBoxes = document.createElement("li");
+              inputBoxes.innerHTML =
+                "<p>" +
+                listOfBooks[i].title +
+                "</p> <input id='textbox" +
+                listOfBooks[i].id +
+                "' value='" +
+                listOfBooks[i].quantity +
+                "' type='text' ></input> <input onclick='btn(" +
+                listOfBooks[i].id +
+                ")' type='button' value='save'></input>";
+              list.appendChild(inputBoxes);
+            }
+            document.getElementById("root").appendChild(list);
+          }
+          
+          buildAdminList();
+          
+          // 
+        async function btn(id) {
+            console.log(id);
+        const send = await fetch("http://localhost:3001/updateBook", {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+              id: id,
+              quantity: document.getElementById("textbox" + id).value,
+              }),
+            });
+          }
